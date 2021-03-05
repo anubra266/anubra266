fbe64723448e8a77e36d9a9f5c60b44cd696dec0
anubra266 <anubra266@gmail.com>
2021-03-05 11:40:13 +0100

    ♿️ Hide credits on small screens

--- components/layout/slant.tsx
+++ components/layout/slant.tsx
@@ -1,22 +1,25 @@
-import { chakra, Box, Stack } from "@chakra-ui/react";
+import { chakra, Box, Stack, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
 
 const Slant = () => {
+  const [isTallEnough] = useMediaQuery("(min-height: 560px)");
   return (
-    <Stack
-      transform="rotate(-90deg)"
-      transformOrigin="left top"
-      whiteSpace="nowrap"
-      pos="absolute"
-      bottom="-55px"
-      letterSpacing="0.8px"
-      lineHeight="80px"
-    >
-      <Box>
-        <chakra.span color="brand.300"> Abraham Anuoluwapo A.</chakra.span>{" "}
-        &copy; 2021
-      </Box>
-    </Stack>
+    isTallEnough && (
+      <Stack
+        transform="rotate(-90deg)"
+        transformOrigin="left top"
+        whiteSpace="nowrap"
+        pos="absolute"
+        bottom="-55px"
+        letterSpacing="0.8px"
+        lineHeight="80px"
+      >
+        <Box>
+          <chakra.span color="brand.300"> Abraham Anuoluwapo A.</chakra.span>{" "}
+          &copy; 2021
+        </Box>
+      </Stack>
+    )
   );
 };
 
