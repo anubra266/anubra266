204360300080575a6c6ea743a609e3655b7365ff
anubra266 <anubra266@gmail.com>
2021-03-06 23:09:22 +0100

    ⚡️ Finish first portfolio launch

--- components/page/contact/form.tsx
+++ components/page/contact/form.tsx
@@ -3,7 +3,10 @@ import { Input, InputProps } from "@chakra-ui/input";
 import { Stack } from "@chakra-ui/layout";
 import { chakra } from "@chakra-ui/system";
 import { Textarea, TextareaProps } from "@chakra-ui/textarea";
-import React from "react";
+import React, { useState } from "react";
+import * as emailjs from "emailjs-com";
+import { useForm } from "./useForm";
+import { useToast, UseToastOptions } from "@chakra-ui/toast";
 
 const Form = () => {
   const inputStyles: InputProps & TextareaProps = {
@@ -17,20 +20,93 @@ const Form = () => {
     _placeholder: {
       color: "whiteAlpha.600",
     },
-    isRequired: true,
   };
+  const toast = useToast();
+  const announce = (mes: string, stat: UseToastOptions["status"]) => {
+    toast({
+      description: mes,
+      status: stat,
+      duration: 5000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+  const { name, email, subject, message, reset } = useForm({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    let templateParams = {
+      from_name: name.value,
+      from_email: email.value,
+      to_name: process.env.myName,
+      subject: subject.value,
+      message: message.value,
+    };
+    emailjs
+      .send(
+        "service_lpbnls3",
+        "template_1qcfjt3",
+        templateParams,
+        "user_Q4UfbP80V0crFqe5nhEHd"
+      )
+      .then(
+        () => {
+          reset();
+          announce("Your message was sent successfully", "success");
+          setLoading(false);
+        },
+        (err) => {
+          console.log("err :>> ", err);
+          announce(`${err}. Please try again`, "error");
+        }
+      );
+  };
+
   return (
-    <chakra.form mb={8}>
+    <chakra.form mb={8} onSubmit={handleSubmit}>
       <Stack spacing={8}>
-        <Input placeholder="Name" type="text" {...inputStyles} />
-        <Input placeholder="Email" type="email" {...inputStyles} />
-        <Input placeholder="Subject" type="subject" {...inputStyles} />
-        <Textarea placeholder="Your Message" {...inputStyles} rows={5} pt={6} />
+        <Input
+          placeholder="Name"
+          name="name"
+          type="text"
+          {...inputStyles}
+          {...name}
+        />
+        <Input
+          placeholder="Email"
+          name="email"
+          type="email"
+          {...inputStyles}
+          {...email}
+        />
+        <Input
+          placeholder="Subject"
+          name="subject"
+          type="text"
+          {...inputStyles}
+          {...subject}
+        />
+        <Textarea
+          placeholder="Your Message"
+          name="message"
+          {...inputStyles}
+          rows={5}
+          pt={6}
+          {...message}
+        />
         <Button
           size="lg"
           textTransform="uppercase"
           w={[, "fit-content"]}
           py={8}
+          type="submit"
+          isLoading={loading}
         >
           Send Message
         </Button>
---
+++ components/page/contact/useForm.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export const useForm = (initialValues: any): any => {
+  const [data, setData] = useState(initialValues);
+  const reset = () => setData(initialValues);
+  const fields = Object.keys(initialValues).reduce((acc, nxt) => {
+    const props = {
+      value: data[nxt],
+      onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.persist();
+        setData((d: any) => ({
+          ...d,
+          [e.target.name]: e.target.value,
+        }));
+      },
+    };
+    acc[nxt] = props;
+    return acc;
+  }, {});
+  return { ...fields, reset };
+};
