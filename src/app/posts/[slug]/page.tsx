120447edb491771b6c100db0c9cc7b94990ae58a
anubra266 <anubra266@gmail.com>
2024-02-27 22:33:43 -0600

    Initial commit

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,103 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getAllPosts, getPostBySlug } from "../../../lib/api";
+import { CMS_NAME } from "../../../lib/constants";
+import markdownToHtml from "../../../lib/markdownToHtml";
+import DateFormatter from "@/app/components/date-formatter";
+import { css, cva } from "styled-system/css";
+
+export default async function Post({ params }: Params) {
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const content = await markdownToHtml(post.content || "");
+
+  return (
+    <main>
+      <article className={css({ mb: "32" })}>
+        <span>{post.title}</span>
+
+        <div
+          className={css({
+            mb: "8",
+            md: { mb: "16" },
+            sm: { ml: "0", mr: "0" },
+          })}
+        >
+          <img src={post.coverImage} alt={post.title} />
+        </div>
+        <div className={css({ maxW: "2xl", ml: "auto", mr: "auto" })}>
+          <div className={css({ mb: "6", fontSize: "lg", lineHeight: "lg" })}>
+            <DateFormatter dateString={post.date} />
+          </div>
+        </div>
+
+        <div className={css({ maxW: "2xl", ml: "auto", mr: "auto" })}>
+          <div
+            className={markdown()}
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        </div>
+      </article>
+    </main>
+  );
+}
+
+const markdown = cva({
+  base: {
+    fontSize: "lg",
+    lineHeight: "relaxed",
+
+    "& p, & ul, & ol, & blockquote": {
+      my: "6",
+    },
+
+    "& h2": {
+      fontSize: "3xl",
+      lineHeight: "snug",
+      mt: "12",
+      mb: "4",
+    },
+
+    "& h3": {
+      fontSize: "2xl",
+      lineHeight: "snug",
+      mt: "8",
+      mb: "4",
+    },
+  },
+});
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateMetadata({ params }: Params): Metadata {
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+
+  return {
+    openGraph: {
+      title,
+      images: [post.ogImage.url],
+    },
+  };
+}
+
+export async function generateStaticParams() {
+  const posts = getAllPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import { getAllPosts } from "../../lib/api";
+import DateFormatter from "@/app/components/date-formatter";
+import { css } from "styled-system/css";
+
+export default function Index() {
+  const posts = getAllPosts();
+
+  return (
+    <main>
+      <section>
+        <h2
+          className={css({
+            mb: "8",
+            fontSize: "5xl",
+            lineHeight: "tight",
+            md: { fontSize: "7xl", lineHeight: "7xl" },
+            fontWeight: "bold",
+            letterSpacing: "tighter",
+          })}
+        >
+          More Stories
+        </h2>
+        <div
+          className={css({
+            display: "grid",
+            gridTemplateColumns: "cols.1",
+            md: {
+              gridTemplateColumns: "cols.2",
+              columnGap: "x.16",
+              rowGap: "y.32",
+            },
+            lg: { columnGap: "x.32" },
+            rowGap: "y.20",
+            mb: "32",
+          })}
+        >
+          {posts.map((post) => (
+            <div key={post.slug}>
+              <h3
+                className={css({
+                  fontSize: "3xl",
+                  lineHeight: "snug",
+                  mb: "3",
+                })}
+              >
+                <Link
+                  as={`/posts/${post.slug}`}
+                  href="/posts/[slug]"
+                  className={css({
+                    _hover: { textDecorationLine: "underline" },
+                  })}
+                >
+                  {post.title}
+                </Link>
+              </h3>
+              <div
+                className={css({ fontSize: "lg", lineHeight: "lg", mb: "4" })}
+              >
+                <DateFormatter dateString={post.date} />
+              </div>
+              <p
+                className={css({
+                  fontSize: "lg",
+                  lineHeight: "relaxed",
+                  mb: "4",
+                })}
+              >
+                {post.excerpt}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
