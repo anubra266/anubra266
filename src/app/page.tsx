97803b1c829de8a18fb6a80226972636d2c4aa65
anubra266 <anubra266@gmail.com>
2024-03-03 17:54:31 -0600

    Init index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,68 +9,7 @@ export default function Index() {
   return (
     <main>
       <section>
-        <h2
-          className={css({
-            mb: "8",
-            fontSize: "5xl",
-            lineHeight: "tight",
-            md: { fontSize: "7xl", lineHeight: "tight" },
-            fontWeight: "bold",
-            letterSpacing: "tighter",
-          })}
-        >
-          More Stories
-        </h2>
-        <div
-          className={css({
-            display: "grid",
-            gridTemplateColumns: "1",
-            md: {
-              gridTemplateColumns: "2",
-              columnGap: "16",
-              rowGap: "32",
-            },
-            lg: { columnGap: "32" },
-            rowGap: "20",
-            mb: "32",
-          })}
-        >
-          {posts.map((post) => (
-            <div key={post.slug}>
-              <h3
-                className={css({
-                  fontSize: "3xl",
-                  lineHeight: "snug",
-                  mb: "3",
-                })}
-              >
-                <Link
-                  as={`/posts/${post.slug}`}
-                  href="/posts/[slug]"
-                  className={css({
-                    _hover: { textDecorationLine: "underline" },
-                  })}
-                >
-                  {post.title}
-                </Link>
-              </h3>
-              <div
-                className={css({ fontSize: "lg", lineHeight: "snug", mb: "4" })}
-              >
-                <DateFormatter dateString={post.date} />
-              </div>
-              <p
-                className={css({
-                  fontSize: "lg",
-                  lineHeight: "relaxed",
-                  mb: "4",
-                })}
-              >
-                {post.excerpt}
-              </p>
-            </div>
-          ))}
-        </div>
+        
       </section>
     </main>
   );
