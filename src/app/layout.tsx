af96b8ffcf4e96ab729d1ed662f4c3c3e476c567
anubra266 <anubra266@gmail.com>
2024-03-23 00:37:48 -0500

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,11 +10,56 @@ import { Footer } from "~/components/layout/footer";
 import { flex } from "styled-system/patterns";
 import { Providers } from "~/app/providers";
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
+  themeColor: "#000",
+};
+
 export const metadata: Metadata = {
   title: `Abraham A. Aremu (aka anubra266) - Software Engineer and Open Source Enthusiast`,
   description: `Software engineer passionate about UI, accessibility and DX.`,
   openGraph: {
     images: ["https://portfolio-anubra266.vercel.app/api/og"],
+  },
+  manifest: "/favicon/site.webmanifest",
+  metadataBase: new URL("https://anubra266.dev"),
+
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      url: "https://avatars.githubusercontent.com/u/30869823?s=180&v=4",
+      sizes: "180x180",
+    },
+    {
+      rel: "icon",
+      url: "https://avatars.githubusercontent.com/u/30869823?s=32&v=4",
+      type: "image/png",
+      sizes: "32x32",
+    },
+    {
+      rel: "icon",
+      url: "https://avatars.githubusercontent.com/u/30869823?s=16&v=4",
+      type: "image/png",
+      sizes: "16x16",
+    },
+    { rel: "shortcut icon", url: "/favicon.ico" },
+    { rel: "mask-icon", url: "/favicon/safari-pinned-tab.svg", color: "#000" },
+  ],
+  twitter: {
+    site: "@anubra266",
+    card: "summary_large_image",
+    title:
+      "Abraham A. Aremu (aka anubra266) - Software Engineer and Open Source Enthusiast",
+    description: "Software engineer passionate about UI, accessibility and DX.",
+    creator: "@anubra266",
+    images: ["https://portfolio-anubra266.vercel.app/api/og"],
+  },
+  alternates: {
+    types: {
+      "application/rss+xml": "/feed.xml",
+    },
   },
 };
 
@@ -24,64 +70,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="https://avatars.githubusercontent.com/u/30869823?s=180&v=4"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="https://avatars.githubusercontent.com/u/30869823?s=32&v=4"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="https://avatars.githubusercontent.com/u/30869823?s=16&v=4"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:domain"
-          content="portfolio-anubra266.vercel.app"
-        />
-        <meta property="twitter:url" content="https://x.com/anubra266" />
-        <meta
-          name="twitter:title"
-          content="Abraham A. Aremu (aka anubra266) - Software Engineer and Open Source Enthusiast"
-        />
-        <meta
-          name="twitter:description"
-          content="Software engineer passionate about UI, accessibility and DX."
-        />
-        <meta
-          name="twitter:image"
-          content="https://portfolio-anubra266.vercel.app/api/og"
-        />
-
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-        />
-
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link
-          rel="shortcut icon"
-          href="https://avatars.githubusercontent.com/u/30869823?s=16&v=4"
-        />
-        <meta name="msapplication-TileColor" content="#000000" />
-
-        <meta name="theme-color" content="#000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      </head>
       <body
         style={
           {
@@ -93,10 +81,8 @@ export default function RootLayout({
           <div
             className={flex({
               direction: "column",
-              // mx: "auto",
               align: "center",
               minH: "screen",
-              // maxW: "[var(--maxW)]",
               "--maxW": "1288px",
               "--padding": {
                 base: "spacing.4",
@@ -111,6 +97,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
