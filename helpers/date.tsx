c013277ed140de29bbd4d9108bc77b98f59d6417
anubra266 <anubra266@gmail.com>
2024-06-26 00:53:46 -0500

    chore: remove site

--- helpers/date.tsx
+++
@@ -1,3 +0,0 @@
-export const diffYears = (date: string) => {
-  return new Date().getFullYear() - parseInt(date);
-};
--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import Theme from "theme";
-import { RootApp } from "types";
-
-function MyApp({ Component, pageProps }: RootApp) {
-  const layout = Component.layout || ((page: any) => page);
-  return <Theme>{layout(<Component {...pageProps}></Component>)}</Theme>;
-}
-
-export default MyApp;
